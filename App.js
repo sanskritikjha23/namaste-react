348c98b2130e8d9d4200a3c4b626b74e3e448fd5
Sanskriti <sanskritijha434@gmail.com>
2024-06-25 11:38:39 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,6 @@
+const create = React.createElement(
+    "h1",
+     {id: "heading" },
+     "I will complete react in just 2 days" );
+        const root  =ReactDOM.createRoot(document.getElementById("root"));
+        root.render(create);
